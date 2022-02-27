ad92c0f40ef28d03ebcdc516f8c012db2d5e63b3
amani-joseph <mr.amanijoseph@gmail.com>
2022-02-27 14:35:48 +0300

    App structure, News class and API KEY added

---
+++ app/model.py
@@ -0,0 +1,11 @@
+class News:
+    '''
+    News Class  to define my news objects
+    '''
+
+    def __init__(self,title,description,urlToImage,content,publishedAt):
+        self.title = title
+        self.description = description
+        self.urlToImage = urlToImage
+        self.content = content
+        self.publishedAt = publishedAt
---
+++ instance/config.py
@@ -0,0 +1,1 @@
+NEWS_API_KEY = '38809fe776c348fead625faede1c2513'
