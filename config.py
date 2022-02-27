330fba5f1fc6954763d924099207ecd07552b52d
amani-joseph <mr.amanijoseph@gmail.com>
2022-02-27 17:19:29 +0300

    Added the requisite configurations

---
+++ config.py
@@ -0,0 +1,34 @@
+import os
+
+class Config:
+     '''
+     General configuration parent class
+     '''
+     NEWS_API_BASE_URL ='https://newsapi.org/v2/top-headlines?country=us&apiKey=38809fe776c348fead625faede1c2513'
+     NEWS_API_KEY = os.environ.get('38809fe776c348fead625faede1c2513')
+     
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
+    
+config_options = {
+     'development': DevConfig,
+     'production' : ProdConfig
+}
